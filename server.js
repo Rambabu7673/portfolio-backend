f3e06cfb24aa7a0e8ef89d87176de30d072a0bc1
Rambabu7673 <155122516+Rambabu7673@users.noreply.github.com>
2026-06-18 00:45:06 +0530

    change code

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // Server Port
-const Port = 2000;
+const Port = process.env.PORT || 5000;
 
 app.listen(Port, () => {
   console.log(`Server is running on port ${Port}`);
